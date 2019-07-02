35680d11740e309d861bb082142ca2f089e5d46e
Momchil Hardalov <momchil.hardalov@gmail.com>
2019-07-02 10:32:52 +0300

    Added constructor with arg iteratable from rules, and class method for reading rules from file. Added tests for more scenarios. Fixed docstring.

--- bulstem/stem.py
+++ bulstem/stem.py
@@ -36,6 +36,9 @@ class BulStemmer(StemmerI):
 
     class TrieNode:
         def __init__(self):
+            """
+            Constructs TrieNode.
+            """
             self.stem = ""
             self.chars = {}
 
@@ -43,14 +46,23 @@ class BulStemmer(StemmerI):
             return len(self.stem)
 
     class SuffixTrie:
-        def __init__(self):
+        def __init__(self, allow_duplicates=False):
+            """
+            Constructs SuffixTrie.
+
+            :param allow_duplicates: (Optional) bool, if false it raises ValueError exception when duplicates are found.
+            """
             self._root = BulStemmer.TrieNode()
+            self._allow_duplicates = allow_duplicates
 
         def add(self, word, stem):
             """
-            Adds a single word with it's stem to the SuffixTrie
-            :param word: string, original word, before stemming
-            :param stem: string, corresponding stem
+            Adds a single word with it's stem to the SuffixTrie.
+
+            :param word: string, original word, before stemming.
+            :param stem: string, corresponding stem.
+
+            :raises ValueError: if duplicates are found in the fields.
             """
             curr = self._root
             for c in reversed(word.lower()):
@@ -58,14 +70,19 @@ class BulStemmer(StemmerI):
                     curr.chars[c] = BulStemmer.TrieNode()
                 curr = curr.chars[c]
 
+            if not self._allow_duplicates and curr.is_stem():
+                raise ValueError("Duplicate key '{0}' found".format(stem))
+
             curr.stem = stem
 
         def get(self, word, vowel_idx):
             """
             Finds the longest possible rule from the end of the word, and appends it to the non-stemmed prefix.
-            :param word: string, original word, before stemming
-            :param vowel_idx: int, position of the first vowel
-            :return: string, lower-cased and stemmed version of the word
+
+            :param word: string, original word, before stemming.
+            :param vowel_idx: int, position of the first vowel.
+
+            :return: string, lower-cased and stemmed version of the word.
             """
             word = word.lower()
             stem = ""
@@ -85,37 +102,73 @@ class BulStemmer(StemmerI):
 
             return word[:idx] + stem
 
-    def __init__(self, path, encoding="utf-8", min_freq=2, left_context=3):
+    def __init__(self, rules, min_freq=2, left_context=3, allow_duplicates=False):
+        """
+        Constructs BulStemmer.
+
+        :param rules: Iterable[string], a collection of strings formatted, as follows: word ==> stem freq.
+        :param min_freq: (Optional) int, the minimum frequency of a rule to be used when stemming.
+        :param left_context: (Optional) int, size of the prefix which will not be stemmed.
+        :param allow_duplicates: (Optional) bool, if false it raises ValueError exception when duplicates are found.
+
+        :raises ValueError: if duplicates are found in the fields.
+        """
         self._min_freq = min_freq
         self._left_context = left_context
-        self._stem_rules = self._read_rules(path, encoding=encoding)
+        self._stem_rules = self._read_rules(rules, allow_duplicates)
 
-    def _read_rules(self, path, encoding="utf-8"):
+    @classmethod
+    def from_file(cls, path, encoding="utf-8", min_freq=2, left_context=3, allow_duplicates=False):
         """
-        Fills the Trie with the corresponding rules in the format:
-        word ==> stem ==> freq
-        :param path: string, path to the rules file
+        Constructs BulStemmer from file.
+
+        :param path: string, path to the rules file formatted: word ==> stem freq.
         :param encoding: (Optional) string, encoding of the rules file
-        :return: SuffixTrie, Trie filled with stemming rules
+        :param min_freq: (Optional) int, the minimum frequency of a rule to be used when stemming.
+        :param left_context: (Optional) int, size of the prefix which will not be stemmed.
+        :param allow_duplicates: (Optional) bool, if false it raises ValueError exception when duplicates are found.
+
+        :returns BulStemmer, an instance of BulStemmer.
+        :raises ValueError: if duplicates are found in the fields.
         """
-        stem_rules = BulStemmer.SuffixTrie()
-
         with open(path, 'r', encoding=encoding) as rules:
-            for line in rules:
-                m = BulStemmer.RULES_PATTERN.match(line.strip())
-                if not m or len(m.groups()) != 3:
-                    continue
+            stemmer = cls(rules, min_freq, left_context, allow_duplicates)
 
-                (word, stem, freq) = m.groups()
-                if int(freq) >= self._min_freq:
-                    stem_rules.add(word, stem)
+        return stemmer
+
+    def _read_rules(self, rules, allow_duplicates=False):
+        """
+        Fills the Trie with the corresponding rules
+
+        :param rules: Iterable[string], a collection of strings formatted, as follows: word ==> stem freq.
+        :param allow_duplicates: (Optional) bool, if false it raises ValueError exception when duplicates are found.
+
+        :return: SuffixTrie, Trie filled with stemming rules.
+        :raises ValueError: if duplicates are found in the fields.
+        """
+        stem_rules = BulStemmer.SuffixTrie(allow_duplicates)
+        for line in rules:
+            m = BulStemmer.RULES_PATTERN.match(line.strip())
+            if not m or len(m.groups()) != 3:
+                continue
+
+            (word, stem, freq) = m.groups()
+            if int(freq) >= self._min_freq:
+                stem_rules.add(word, stem)
 
         return stem_rules
 
     @staticmethod
-    def pos_first_vowel(stem):
+    def pos_first_vowel(token):
+        """
+        Finds the position of the first vowel in a token.
+
+        :param token: string, a lower-cased word.
+
+        :return: string, position of the first vowel, if found, else one position after the last index.
+        """
         i = 0
-        while i < len(stem) and stem[i] not in BulStemmer.VOWELS:
+        while i < len(token) and token[i] not in BulStemmer.VOWELS:
             i += 1
 
         return i
@@ -124,13 +177,18 @@ class BulStemmer(StemmerI):
         """
         The stemming is performed by applying the longest possible rule (if any), provided that the stem produced
         contains at least one vowel.
-        :param token: string, token to be stemmed
-        :return: string, stem of the word
+
+        :param token: string, token to be stemmed.
+
+        :return: string, stem of the word.
         """
         stem = token.lower()
         if len(stem) > self._left_context:
-            # There must be at least one vowel in the resultant stem, hence we stem everything after the first one
+            # There must be at least one vowel in the resultant stem, hence we stem everything after the first one.
             i = BulStemmer.pos_first_vowel(stem) + 1
             stem = self._stem_rules.get(stem, i)
 
         return stem
+
+stemmer = BulStemmer(["ой ==> о 10"], min_freq=0, left_context=0)
+stemmer.stem('тоой')
--- tests/bulstem_test.py
+++ tests/bulstem_test.py
@@ -9,10 +9,12 @@ from bulstem.stem import BulStemmer
 
 
 class BulStemmerTest(unittest.TestCase):
-    RULES_PATH = './rules/stem_rules_context_2_utf8.txt'
+    RULES_1_PATH = './rules/stem_rules_context_1_utf8.txt'
+    RULES_2_PATH = './rules/stem_rules_context_2_utf8.txt'
+    RULES_3_PATH = './rules/stem_rules_context_3_utf8.txt'
 
     def test_full_stem(self):
-        stemmer = BulStemmer(BulStemmerTest.RULES_PATH, min_freq=2, left_context=3)
+        stemmer = BulStemmer.from_file(BulStemmerTest.RULES_2_PATH, min_freq=2, left_context=2)
         text = \
             '''
             Има първи вероятен случай на атипична пневмония в България, съобщи министърът на здравеопазването Божидар 
@@ -36,8 +38,45 @@ class BulStemmerTest(unittest.TestCase):
             self.assertEqual(stem, stemmer.stem(token))
 
     def test_stem(self):
-        stemmer = BulStemmer(BulStemmerTest.RULES_PATH, min_freq=2, left_context=3)
+        stemmer = BulStemmer.from_file(BulStemmerTest.RULES_2_PATH, min_freq=2, left_context=2)
         self.assertEqual('вероят', stemmer.stem('вероятен'))
+        self.assertEqual('този', stemmer.stem('този'))
+
+    def test_no_stem_context(self):
+        stemmer = BulStemmer.from_file(BulStemmerTest.RULES_2_PATH, left_context=10)
+        self.assertEqual('оставката', stemmer.stem('оставката'))
+
+    def test_stem_context1(self):
+        stemmer = BulStemmer.from_file(BulStemmerTest.RULES_1_PATH, min_freq=2, left_context=1)
+        self.assertEqual('остав', stemmer.stem('оставката'))
+        self.assertEqual('той', stemmer.stem('той'))
+
+    def test_stem_context2(self):
+        stemmer = BulStemmer.from_file(BulStemmerTest.RULES_2_PATH, min_freq=2, left_context=2)
+        self.assertEqual('оставк', stemmer.stem('оставката'))
+        self.assertEqual('той', stemmer.stem('той'))
+
+    def test_stem_context3(self):
+        stemmer = BulStemmer.from_file(BulStemmerTest.RULES_3_PATH, min_freq=2, left_context=3)
+        self.assertEqual('оставк', stemmer.stem('оставката'))
+        self.assertEqual('той', stemmer.stem('той'))
+
+    def test_filter_freq(self):
+        stemmer = BulStemmer.from_file(BulStemmerTest.RULES_3_PATH, min_freq=200000000)
+        self.assertEqual('оставката', stemmer.stem('оставката'))
+        self.assertEqual('вероятен', stemmer.stem('вероятен'))
+
+    def test_manual_rules(self):
+        stemmer = BulStemmer(["ой ==> о 10"], min_freq=0, left_context=0)
+        self.assertEqual('поро', stemmer.stem('порой'))
+
+    def test_allow_duplicates(self):
+        stemmer = BulStemmer(["ой ==> о 10", "ой ==> о 30"], min_freq=0, left_context=0, allow_duplicates=True)
+        self.assertEqual('поро', stemmer.stem('порой'))
+
+    def test_duplicates_exception(self):
+        with self.assertRaises(ValueError):
+            BulStemmer(["ой ==> о 10", "ой ==> о 30"], min_freq=0, left_context=0)
 
 
 if __name__ == '__main__':
